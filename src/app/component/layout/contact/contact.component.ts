82b6ea0843dc4a71b112dbff2c6de26daea65b92
ashrafTSS <ashraf@tecnovators.in>
2023-06-09 16:55:15 +0530

    validation

--- src/app/component/layout/contact/contact.component.ts
+++ src/app/component/layout/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { TranslateService } from '@ngx-translate/core';
 import { ContactService } from 'src/app/service/contact.service';
@@ -12,42 +12,49 @@ import { ContactService } from 'src/app/service/contact.service';
 export class ContactComponent implements OnInit{
 
   //contact form
-  ContactForm = this.fb.group({
-    fullName: ['',Validators.required],
-    emailAddress: ['',[Validators.required, Validators.email]],
-    phone: ['',Validators.required],
-    message: ['',Validators.required],
-  });
+  // ContactForm = this.fb.group({
+  //   fullName: ['',Validators.required],
+  //   emailAddress: ['',[Validators.required, Validators.email]],
+  //   phone: ['',Validators.required],
+  //   message: ['',Validators.required],
+  // });
+  contactForm!: FormGroup;
 
-  constructor(private translate:TranslateService,private fb: NonNullableFormBuilder,
-    private contactService:ContactService,private toast:HotToastService){}
+  constructor(private translate:TranslateService,
+    private contactService:ContactService,private toast:HotToastService,
+    private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
-
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+    });
   }
 
-  get fullName() {
-    return this.ContactForm.get('fullName');
-  }
+  // get fullName() {
+  //   return this.ContactForm.get('fullName');
+  // }
 
-  get emailAddress() {
-    return this.ContactForm.get('emailAddress');
-  }
+  // get emailAddress() {
+  //   return this.ContactForm.get('emailAddress');
+  // }
 
-  get phone() {
-    return this.ContactForm.get('phone');
-  }
+  // get phone() {
+  //   return this.ContactForm.get('phone');
+  // }
 
-  get message() {
-    return this.ContactForm.get('message');
-  }
+  // get message() {
+  //   return this.ContactForm.get('message');
+  // }
 
   //save contact
   saveContact(){
-      this.contactService.addUser(this.ContactForm.value)
+      this.contactService.addUser(this.contactForm.value)
       .then(() =>{
         this.toast.success("Successfully send your message")
-        this.ContactForm.reset()
+        this.contactForm.reset()
       })
       .catch(()=>{
          this.toast.error("there is a send your message")
